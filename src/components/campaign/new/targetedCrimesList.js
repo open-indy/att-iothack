66e971b71e7f6fda182906502704c2994ce39138
Jonathon Hibbard <infolock@gmail.com>
2016-04-30 16:06:57 -0400

    Resolving left-over conflicts

--- src/components/campaign/new/targetedCrimesList.js
+++ src/components/campaign/new/targetedCrimesList.js
@@ -2,35 +2,13 @@ import React from 'react';
 import Label from '../../../common/form/label';
 import SelectList from '../../../common/form/select';
 
-<<<<<<< HEAD
-=======
-const crimes = [
-  {
-    value: 'drug-related',
-    label: 'Drug-related'
-  },
-  {
-    value: 'burglary',
-    label: 'Burglary'
-  },
-  {
-    value: 'homocide',
-    label: 'Homocide'
-  }
-]
-
->>>>>>> master
 const TargetedCrimesList = (props) => (
   <div>
     <Label text="Target Campaign:" />
     <SelectList
       name="targeted-crimes"
       defaultOption="homocide"
-<<<<<<< HEAD
       options={ props.crimes }
-=======
-      options={ crimes }
->>>>>>> master
     />
   </div>
 );
