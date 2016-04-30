1e742cebc7f80c6a185bae48380eb66cfd069b3c
Jonathon Hibbard <infolock@gmail.com>
2016-04-30 13:53:11 -0400

    First draft of create campaign screen

--- src/components/campaign/create.js
+++ src/components/campaign/create.js
@@ -2,10 +2,64 @@ import React, { PropTypes } from 'react';
 
 const CreateCampaign = (props) => (
   <div>
-    <div>
-      <label>Name:</label>
-      <input type="text" name="name" id="campaign-name" class="create-campaign" value="" />
-    </div>
+    <ul>
+      <li>
+        <label>Name:</label>
+        <input type="text" name="name" id="campaign-name" class="create-campaign" value="" />
+      </li>
+      <li>
+        <label>Target Campaign:</label>
+        <select>
+          <option>Homocide</option>
+          <option>Drug-related</option>
+          <option>Burglary</option>
+        </select>
+      </li>
+      <li>
+        <label>Target Area(s)</label>
+        <ul>
+          <li>
+            <input type="checkbox" name="targetArea[]" value="Brownsburg" /> Brownsburg, IN
+          </li>
+          <li>
+            <input type="checkbox" name="targetArea[]" value="Downtown-Indianapolis" /> Indianapolis, IN (downtown)
+          </li>
+        </ul>
+      </li>
+      <li>
+        <label>Control Area(s)</label>
+        <ul>
+          <li>
+            <input type="checkbox" name="controlArea[]" value="Brownsburg" /> Brownsburg, IN
+          </li>
+          <li>
+            <input type="checkbox" name="controlArea[]" value="Downtown-Indianapolis" /> Indianapolis, IN (downtown)
+          </li>
+        </ul>
+      </li>
+      <li>
+        <label>Goals</label>
+        <ul>
+          <li>
+            <input type="text" name="goal" value="" alt="Enter a goal" />
+            <button>Add</button>
+          </li>
+        </ul>
+      </li>
+      <li>
+        <label>Strategies</label>
+        <ul>
+          <li>
+            <input type="text" name="strategy" value="" alt="Enter a strategy" />
+            <button>Add</button>
+          </li>
+        </ul>
+        Suggested Strategies for the Target Areas and Targeted Crime based on previous campaigns:
+        <ul>
+          <li>No Data Available.</li>
+        </ul>
+      </li>
+    </ul>
   </div>
 );
 
